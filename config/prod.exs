33294447ffbff72fbb6613ca273630fcfeb65185
awwyushh <awwwyushh@gmail.com>
2024-12-27 17:12:15 +0530

    [+] Added domain resolution

--- config/prod.exs
+++ config/prod.exs
@@ -5,3 +5,8 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+config :shader_api, ShaderApiWeb.Endpoint,
+  url: [host: "shadergen-api.onrender.com", scheme: "https", port: 443],
+  http: [ip: {0, 0, 0, 0}, port: String.to_integer(System.get_env("PORT") || "4000")],
+  server: true,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
